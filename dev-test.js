cdb60c7afd8afc572527859ca3239c0f1cd46d5e
limyang51 <limyang51@gmail.com>
2018-05-19 19:48:36 +0800

    first commit

---
+++ dev-test.js
@@ -0,0 +1,9 @@
+const Block = require('./block');
+
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
+
+// const block = new Block('foo', 'bar', 'zoo', 'baz');
+// console.log(block.toString());
+// console.log(Block.genesis().toString());
+
